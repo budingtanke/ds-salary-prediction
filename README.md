004bc731adea8cb30720e4e5a8f4564488a772c5
Han Lin <hl3006@columbia.edu>
2020-10-11 17:21:44 -0400

    glassdoor scraper

---
+++ README.md
@@ -0,0 +1,3 @@
+# Data Scientist Salary Prediction## References
+* [Youtube Walkthrough](https://github.com/PlayingNumbers/ds_salary_proj)
+* [selenium tutorial glassdoor on medium](https://towardsdatascience.com/selenium-tutorial-scraping-glassdoor-com-in-10-minutes-3d0915c6d905) - [code](https://github.com/arapfaik/scraping-glassdoor-selenium/blob/master/glassdoor%20scraping.ipynb)
---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 11 17:03:37 2020
+
+@author: Han
+"""
+
+import glassdoor_scraper as gs
+import pandas as pd
+
+
+PATH = "/Users/linhan/Desktop/Han/Learning/Python/Softwares/chromedriver"
+
+#This line will open a new chrome window and start the scraping.
+df = gs.get_jobs("data scientist", 1000, False, PATH, 10)
+df.to_csv('glassdoor_raw.csv', index=False)
